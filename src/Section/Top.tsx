8366e847971cbb2c9d47dcb80e79726f91c57918
Ricardo Sanchez <ricardosancheze@outlook.com>
2020-04-29 12:49:36 +0200

    Remove demo contact info

--- src/Section/Top.tsx
+++ src/Section/Top.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext, Fragment } from "react";
 import { Segment, Header, Item, Icon } from "semantic-ui-react";
 import photo from "./Top/photo300.jpg";
+import { StoreContext } from "../Store";
 
 export const Top = () => {
+  const { isWeb, phone, email } = useContext(StoreContext);
   return (
     <Segment color="orange">
       <Item.Group>
@@ -12,12 +14,16 @@ export const Top = () => {
             <Header as="h1">Ricardo Sanchez Encinas</Header>
             <Item.Meta>Software developer</Item.Meta>
             <Item.Description>
-              <p>
-                <Icon name="phone" /> +48 733 946 056
-              </p>
-              <p>
-                <Icon name="mail" /> ricardosancheze@outlook.com
-              </p>
+              {isWeb ? null : (
+                <Fragment>
+                  <p>
+                    <Icon name="phone" /> {phone}
+                  </p>
+                  <p>
+                    <Icon name="mail" /> {email}
+                  </p>
+                </Fragment>
+              )}
               <p>
                 <Icon name="linkedin" />{" "}
                 <a href="https://www.linkedin.com/in/ricardo-sanchez-en/">
--- src/Store.ts
+++ src/Store.ts
@@ -1,7 +1,11 @@
 import { createContext } from "react";
 
+const urlParams = new URLSearchParams(window.location.search);
+
 const Store = {
   isWeb: !window.location.search.includes("pdf=true"),
+  email: urlParams.get("email"),
+  phone: urlParams.get("phone"),
 };
 
 export const StoreContext = createContext(Store);
